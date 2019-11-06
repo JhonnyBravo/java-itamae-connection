e34e61e986e8f85d7b17dc4ad364a1089aaa01b3
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-11-06 21:30:32 +0900

    new file: src/main/java/java_itamae_connection/domain/service/connection/ConnectionService.java

---
+++ src/main/java/java_itamae_connection/domain/service/connection/ConnectionService.java
@@ -0,0 +1,29 @@
+package java_itamae_connection.domain.service.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import java_itamae_connection.domain.model.ConnectionInfo;
+
+public abstract class ConnectionService {
+    /**
+     * @param cnInfo 設定ファイルの情報を納めた ConnectionInfo を指定する。
+     * @return connection DB 接続を返す。
+     * @throws Exception {@link java.lang.Exception}
+     */
+    public Connection getConnection(ConnectionInfo cnInfo) throws Exception {
+        final StringBuilder buffer = new StringBuilder();
+
+        buffer.append("jdbc:mysql://");
+        buffer.append(cnInfo.getHostName() + ":");
+        buffer.append(cnInfo.getPortNumber() + "/");
+        buffer.append(cnInfo.getDbName());
+        buffer.append("?characterEncoding=" + cnInfo.getEncoding());
+        buffer.append("&serverTimezone=" + cnInfo.getTimeZone());
+
+        final String cnStr = new String(buffer);
+        final Connection connection = DriverManager.getConnection(cnStr, cnInfo.getUserName(), cnInfo.getPassword());
+
+        return connection;
+    }
+}
