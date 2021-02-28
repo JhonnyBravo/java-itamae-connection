440868c86d8197b93a601fcf78a90c07d6dbf167
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-03-01 01:36:16 +0900

    modified: src/test/java/java_itamae_connection/domain/service/connection_info/ConnectionInfoServiceTest.java
    パッケージのバージョンアップに伴う import の修正。

--- src/test/java/java_itamae_connection/domain/service/connection_info/ConnectionInfoServiceTest.java
+++ src/test/java/java_itamae_connection/domain/service/connection_info/ConnectionInfoServiceTest.java
@@ -1,7 +1,8 @@
 package java_itamae_connection.domain.service.connection_info;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.junit.Before;
 import org.junit.Test;
