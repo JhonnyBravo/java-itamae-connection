4c005f9701ed7a718e10f99edb3118d60a5faed5
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-04-21 00:02:59 +0900

    modified: src/main/java/java_itamae_connection/domain/service/connection_info/ConnectionInfoServiceImpl.java
    デフォルト値の設定処理とバリデーション処理を
    ConnectionInfo クラスと IsValidConnectionInfo クラスへ分離。

--- src/main/java/java_itamae_connection/domain/service/connection_info/ConnectionInfoServiceImpl.java
+++ src/main/java/java_itamae_connection/domain/service/connection_info/ConnectionInfoServiceImpl.java
@@ -17,7 +17,7 @@ public class ConnectionInfoServiceImpl implements ConnectionInfoService {
     public ConnectionInfo getConnectionInfo(ContentsAttribute attr) throws Exception {
         final StreamRepository sr = new StreamRepositoryImpl();
         final PropertiesRepository pr = new PropertiesRepositoryImpl();
-        Map<String, String> properties = new HashMap<String, String>();
+        Map<String, String> properties = new HashMap<>();
 
         try (Reader reader = sr.getReader(attr)) {
             properties = pr.getProperties(reader);
@@ -25,49 +25,13 @@ public class ConnectionInfoServiceImpl implements ConnectionInfoService {
 
         final ConnectionInfo cnInfo = new ConnectionInfo();
 
-        // オプション項目のデフォルト値設定
-        if (properties.get("hostName") == null || properties.get("hostName").isEmpty()) {
-            cnInfo.setHostName("localhost");
-        } else {
-            cnInfo.setHostName(properties.get("hostName"));
-        }
-
-        if (properties.get("portNumber") == null || properties.get("portNumber").isEmpty()) {
-            cnInfo.setPortNumber("3306");
-        } else {
-            cnInfo.setPortNumber(properties.get("portNumber"));
-        }
-
-        if (properties.get("encoding") == null || properties.get("encoding").isEmpty()) {
-            cnInfo.setEncoding("UTF-8");
-        } else {
-            cnInfo.setEncoding(properties.get("encoding"));
-        }
-
-        if (properties.get("timeZone") == null || properties.get("timeZone").isEmpty()) {
-            cnInfo.setTimeZone("JST");
-        } else {
-            cnInfo.setTimeZone(properties.get("timeZone"));
-        }
-
-        // 必須項目のチェック
-        if (properties.get("dbName") == null || properties.get("dbName").isEmpty()) {
-            throw new Exception("dbName が設定されていません。 " + attr.getPath() + " を確認してください。");
-        } else {
-            cnInfo.setDbName(properties.get("dbName"));
-        }
-
-        if (properties.get("userName") == null || properties.get("userName").isEmpty()) {
-            throw new Exception("userName が設定されていません。 " + attr.getPath() + " を確認してください。");
-        } else {
-            cnInfo.setUserName(properties.get("userName"));
-        }
-
-        if (properties.get("password") == null || properties.get("password").isEmpty()) {
-            throw new Exception("password が設定されていません。 " + attr.getPath() + " を確認してください。");
-        } else {
-            cnInfo.setPassword(properties.get("password"));
-        }
+        cnInfo.setHostName(properties.get("hostName"));
+        cnInfo.setPortNumber(properties.get("portNumber"));
+        cnInfo.setEncoding(properties.get("encoding"));
+        cnInfo.setTimeZone(properties.get("timeZone"));
+        cnInfo.setDbName(properties.get("dbName"));
+        cnInfo.setUserName(properties.get("userName"));
+        cnInfo.setPassword(properties.get("password"));
 
         return cnInfo;
     }
