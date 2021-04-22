9455f7dd3cac3f46ca7ffcd6a3890d7681129b3a
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-04-22 18:13:57 +0900

    modified: src/main/java/java_itamae_connection/domain/service/connection_info/ConnectionInfoServiceImpl.java
    checkstyle の指摘事項を修正。

--- src/main/java/java_itamae_connection/domain/service/connection_info/ConnectionInfoServiceImpl.java
+++ src/main/java/java_itamae_connection/domain/service/connection_info/ConnectionInfoServiceImpl.java
@@ -3,7 +3,6 @@ package java_itamae_connection.domain.service.connection_info;
 import java.io.Reader;
 import java.util.HashMap;
 import java.util.Map;
-
 import java_itamae_connection.domain.model.ConnectionInfo;
 import java_itamae_contents.domain.model.ContentsAttribute;
 import java_itamae_contents.domain.repository.stream.StreamRepository;
@@ -12,27 +11,26 @@ import java_itamae_properties.domain.repository.properties.PropertiesRepository;
 import java_itamae_properties.domain.repository.properties.PropertiesRepositoryImpl;
 
 public class ConnectionInfoServiceImpl implements ConnectionInfoService {
+  @Override
+  public ConnectionInfo getConnectionInfo(ContentsAttribute attr) throws Exception {
+    final StreamRepository sr = new StreamRepositoryImpl();
+    final PropertiesRepository pr = new PropertiesRepositoryImpl();
+    Map<String, String> properties = new HashMap<>();
 
-    @Override
-    public ConnectionInfo getConnectionInfo(ContentsAttribute attr) throws Exception {
-        final StreamRepository sr = new StreamRepositoryImpl();
-        final PropertiesRepository pr = new PropertiesRepositoryImpl();
-        Map<String, String> properties = new HashMap<>();
-
-        try (Reader reader = sr.getReader(attr)) {
-            properties = pr.getProperties(reader);
-        }
-
-        final ConnectionInfo cnInfo = new ConnectionInfo();
-
-        cnInfo.setHostName(properties.get("hostName"));
-        cnInfo.setPortNumber(properties.get("portNumber"));
-        cnInfo.setEncoding(properties.get("encoding"));
-        cnInfo.setTimeZone(properties.get("timeZone"));
-        cnInfo.setDbName(properties.get("dbName"));
-        cnInfo.setUserName(properties.get("userName"));
-        cnInfo.setPassword(properties.get("password"));
-
-        return cnInfo;
+    try (Reader reader = sr.getReader(attr)) {
+      properties = pr.getProperties(reader);
     }
+
+    final ConnectionInfo cnInfo = new ConnectionInfo();
+
+    cnInfo.setHostName(properties.get("hostName"));
+    cnInfo.setPortNumber(properties.get("portNumber"));
+    cnInfo.setEncoding(properties.get("encoding"));
+    cnInfo.setTimeZone(properties.get("timeZone"));
+    cnInfo.setDbName(properties.get("dbName"));
+    cnInfo.setUserName(properties.get("userName"));
+    cnInfo.setPassword(properties.get("password"));
+
+    return cnInfo;
+  }
 }
