59b0fb650c0fc007539ec2bc221f405ce8d604e8
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-07-09 01:19:32 +0900

    renamed: src/test/java/java_itamae_connection/domain/service/connection_info/{ConnectionInfoServiceTest.java -> TestConnectionInfoService.java}
    CDI Unit を導入。テストを以下のクラスに分割。
    
    * NoOptionSpecified
    * OptionSpecified

---
+++ src/test/java/java_itamae_connection/domain/service/connection_info/TestConnectionInfoService.java
@@ -0,0 +1,13 @@
+package java_itamae_connection.domain.service.connection_info;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * {@link ConnectionInfoService} のテスト
+ */
+@RunWith(Suite.class)
+@SuiteClasses({NoOptionSpecified.class, OptionSpecified.class})
+public class TestConnectionInfoService {
+}
